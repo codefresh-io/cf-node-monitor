92994a7be33fb4245b5bc5b0e69dcd6b2a65a8b2
Kosta Klevensky <kosta@codefresh.io>
2016-07-05 18:23:44 +0300

    added NodeMonitor.updateNodeStatuses

---
+++ NodeMonitor.js
@@ -0,0 +1,194 @@
+'use strict';
+
+var util = require('util');
+var fs = require('fs');
+var path = require('path');
+var Docker = require('dockerode');
+var request = require('request');
+var Q = require('q');
+
+var fromCallback = function (fn) {
+    var deferred = Q.defer();
+    fn(function (err, data) {
+        if (err) {
+            deferred.reject(err);
+        }
+        else {
+            deferred.resolve(data);
+        }
+    });
+    return deferred.promise;
+};
+var consul       = require('consul')({
+    host: 'consul',
+    port: '8500',
+    promisify: fromCallback
+});
+
+class NodeMonitor {
+
+    constructor(){
+        this.consulUpdateInterval = 10000;
+        this.certPath = path.join(__dirname, '..', 'certs');
+        this.caCertFile = path.join(this.certPath, 'ca.pem');
+        this.clientCertFile = path.join(this.certPath, 'cert.pem');
+        this.clientCertKeyFile = path.join(this.certPath, 'key.pem');
+    }
+
+    getDockerSwarm(){
+        var self = this;
+        return consul.health.service({service: 'swarm-man', passing: true})
+        .then(function(srv){
+            if (!srv || srv.length === 0) {
+                return Q.reject(new Error("swarm-man service is not available"));
+            }
+            var addr             = srv[0].Service.Address;
+            var port             = srv[0].Service.Port;
+            return Q.all(
+                [Q.nfcall(fs.readFile, self.caCertFile, 'utf8'),
+                    Q.nfcall(fs.readFile, self.clientCertFile, 'utf8'),
+                    Q.nfcall(fs.readFile, self.clientCertKeyFile, 'utf8')])
+            .spread(function (caCert, clientCert, clientCertKey) {
+                var docker = new Docker({
+                    host: addr,
+                    port: port,
+                    ca: caCert,
+                    cert: clientCert,
+                    key: clientCertKey
+                });
+                return Q.resolve(docker);
+            })
+        })
+    }
+
+    getSwarmStatus() {
+        return this.getDockerSwarm()
+        .then(docker => Q.ninvoke(docker, "info"))
+        .then(function (swarmInfo) {
+
+            // check if docker daemon is swarm
+            if (!swarmInfo.ServerVersion || !swarmInfo.ServerVersion.match(/^swarm.*/)) {
+                return Q.reject(new Error(util.format("server %s:%s is not swarm", docker.host, docker.port)));
+            }
+
+            //  Transpose docker.info into hash with node statuses
+            var statusArr = swarmInfo.SystemStatus;
+            if (!statusArr || !statusArr.length) {
+                return Q.reject(new Error(util.format("Cannot get nodes SystemStatus from %s:%s", docker.host, docker.port)));
+            }
+            // Transform swarm docker.info node
+            var swarmStatus = {};
+            var erroredNodes = {};
+            var i = 0;
+
+            swarmStatus.NodesData = {};
+            swarmStatus.Fails = [];
+            var propertyPrefix = String.fromCharCode(32, 32, 9492, 32);
+            do {
+                //var node = {};
+                if (!statusArr[i][0].startsWith(propertyPrefix) && statusArr[i + 1] && statusArr[i + 1][0] === propertyPrefix + "ID") {
+                    var node = {
+                        hostname: statusArr[i][0].trim(),
+                        address: statusArr[i][1]
+                    };
+                    i++;
+                    do {
+                        var nodeProp = statusArr[i][0].split(propertyPrefix)[1];
+                        if (nodeProp) {
+                            node[nodeProp] = statusArr[i][1];
+                            i++;
+                        }
+                    }
+                    while (nodeProp && i < statusArr.length);
+                    swarmStatus.NodesData[node.address] = node;
+                    if (node.Error && node.Error !== "(none)" || node.Status !== "Healthy") {
+                        swarmStatus.Fails.push({
+                            address: node.address,
+                            hostname: node.hostname,
+                            Error: node.Error,
+                            Status: node.Status
+                        });
+                    }
+                }
+                else {
+                    swarmStatus[statusArr[i][0]] = statusArr[i][1];
+                    i++;
+                }
+            }
+            while (i < statusArr.length);
+
+            return Q.resolve(swarmStatus);
+        })
+        .catch(err => Q.reject(err));
+    }
+
+    updateNodeStatuses(){
+        var checkId = "service:docker-node";
+
+        return Q.all([
+               () => this.getSwarmStatus(),
+               () => consul.health.service({service: 'docker-node'})])
+        .spread(function(swarmStatus, nodeServices){
+            var consulNodeCheckUpdates = [];
+            for (var s in nodeServices){
+                var checkStatusSwarm, checkStatusConsul, checkOutputSwarm, checkOutputConsul;
+
+                // Parse current service status from consul
+                if (s.Checks) {
+                    var nodeCheckArr = s.Checks.filter(c => c.CheckID === checkId);
+                    if (nodeCheckArr.length > 0) {
+                        checkStatusConsul = nodeCheckArr[0].Status;
+                        checkOutputConsul = nodeCheckArr[0].Output;
+                    }
+                }
+
+                // Parse current node status from swarm
+                var nodeRef = util.format('%s:%s', s.Service.Address, s.Service.Port);
+                if (! swarmStatus.NodesData || ! swarmStatus.NodesData[nodeRef]) {
+                    checkStatusSwarm = 'failing';
+                    checkOutputSwarm = 'The node is not in the swarm cluster';
+                }
+                else {
+                    var swarmNode = swarmStatus.NodesData[nodeRef];
+                    if (swarmNode.Error && swarmNode.Error !== "(none)" || swarmNode.Status !== "Healthy") {
+                        checkStatusSwarm = 'failing';
+                        checkOutputSwarm = swarmNode.Error;
+                    }
+                    else {
+                        checkStatusSwarm = 'passing';
+                        checkOutputSwarm = "";
+                    }
+                }
+
+                if (checkStatusSwarm !== checkOutputConsul || checkOutputSwarm !== checkOutputConsul) {
+                    var consulNodeCheck = {
+                        Node: s.Node.Node,
+                        Address: s.Node.Address,
+                        Check: {
+                            Node: s.Node.Node,
+                            CheckID: checkId,
+                            Name: "Docker Node Check",
+                            Notes: "Docker Node Check - cf-node-monitor",
+                            Status: checkStatusSwarm,
+                            Output: checkOutputSwarm
+                        }
+                    }
+                    consulNodeCheckUpdates.push(consulNodeCheck);
+                }
+            }
+            return Q.resolve(consulNodeCheckUpdates);
+        })
+        .then(function(consulNodeCheckUpdates) {
+            return consulNodeCheckUpdates.map(function(consulNodeCheck){
+                return Q.nfcall(request.put, {
+                    headers: {'content-type': 'application/json'},
+                    url: util.format('http://%s:%s/v1/catalog/register', consul.host, consul.port),
+                    body: JSON.stringify(consulNodeCheck)
+                });
+            });
+        })
+        .then();
+
+    }
+}
+module.exports = new NodeMonitor();
--- index.js
+++ index.js
@@ -1,93 +1,23 @@
 var express = require('express');
 // var bodyParser = require('body-parser');
+
 var util = require('util');
-
-var app = express();
-var router  = express.Router();
-
 var fs = require('fs');
 var Docker = require('dockerode');
 var request = require('request');
 var Q = require('q');
 //app.use(bodyParser.json());
+
+var nodeMonitor = require('./NodeMonitor');
+
+var app = express();
+var router  = express.Router();
 app.use('/', router);
 
-function getDockerInfo() {
-    var nodeAddr = '23.251.147.109';
-    var nodePort = "3376";
-    var caCertFile = "/d1/codefresh/.secure/cf-ca/dev/ca.pem";
-    var clientCertFile = "/d1/codefresh/.secure/cf-ca/dev/client-cert.pem";
-    var clientCertKeyFile = "/d1/codefresh/.secure/cf-ca/dev/client-key.pem";
-
-    return Q.all(
-              [Q.nfcall(fs.readFile, caCertFile, 'utf8'),
-               Q.nfcall(fs.readFile, clientCertFile, 'utf8'),
-               Q.nfcall(fs.readFile, clientCertKeyFile, 'utf8')])
-    .spread(function(caCert, clientCert, clientCertKey){
-        var  docker = new Docker({
-            host: nodeAddr,
-            port: nodePort,
-            ca: caCert,
-            cert: clientCert,
-            key: clientCertKey
-        });
-        return Q.resolve(docker);
-    })
-    .then(docker => Q.ninvoke(docker, "info"))
-    .then(function(dockerInfo){
-
-        // check if docker daemon is swarm
-        if (! dockerInfo.ServerVersion || ! dockerInfo.ServerVersion.match(/^swarm.*/)) {
-            return Q.reject(new Error(util.format("server %s:%s is not swarm", nodeAddr, nodePort)));
-        }
-
-        //  Transpose docker.info into hash with node statuses
-        var statusArr = dockerInfo.SystemStatus;
-        if (! statusArr || ! statusArr.length) {
-            return Q.reject(new Error(util.format("Cannot get nodes SystemStatus from %s:%s", nodeAddr, nodePort)));
-        }
-        var systemStatus = {};
-        var erroredNodes = {};
-        var i = 0;
-        // write swarm systemStatus until first node
-        for(;i<statusArr.length; i++) {
-            systemStatus[statusArr[i][0]] = statusArr[i][1];
-            if (statusArr[i][0] === 'Nodes') break;
-        }
-
-
-        // Transform swarm docker.info node
-        i++;
-        systemStatus.NodesData = [];
-        var propertyPrefix = String.fromCharCode(32,32,9492,32);
-        do {
-            var node = {};
-            if (! statusArr[i][0].startsWith(propertyPrefix) && statusArr[i+1] && statusArr[i+1][0] === propertyPrefix + "ID"){
-                node = {name: statusArr[i][0].trim(),
-                          ip: statusArr[i][1]};
-                i++;
-                do {
-                    var nodeProp = statusArr[i][0].split(propertyPrefix)[1];
-                    if (nodeProp) {
-                        node[nodeProp] = statusArr[i][1];
-                        i++;
-                    }
-                }
-                while(nodeProp && i < statusArr.length);
-                systemStatus.NodesData.push(node);
-            }
-            else i++;
-
-        }
-        while (i < statusArr.length);
-
-        return Q.resolve(systemStatus);
-    })
-    .catch(err => Q.reject(err));
-}
 
 router.get('/', function(req, res) {
-   getDockerInfo().then( dockerInfo => res.send(dockerInfo) );
+    //getDockerInfo().then( dockerInfo => res.send(dockerInfo) );
+    nodeMonitor.getSwarmStatus().done( swarmStatus => res.send(swarmStatus), err => res.status(400).send(err.toString())  );
 });
 
 
